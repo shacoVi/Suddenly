daadb6e6ee83b326baaada6e8d8ea410690a5040
shacoVi <46135626+shacoVi@users.noreply.github.com>
2018-12-25 17:52:48 +0100

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+
+
+
+/*
+var Init = {   method: 'GET',
+               headers: {
+                   'Content-Type': 'text/html'
+               },
+               mode: 'cors',
+               cache: 'default'
+};
+
+
+var Requete = new Request('gptbico', Init);
+
+fetch(Requete)
+    .then(function(response) {
+        return response.blob();
+    })
+    .then(function(myBlob) {
+        var objectURL = URL.createObjectURL(myBlob);
+        document.getElementById('iframe1').src = objectURL;
+    });
+*/
